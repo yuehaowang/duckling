6944c1ba3bbc0e722974ac9c85e2dc5c8f3e121b
sunzhg <732409347@qq.com>
2018-01-19 13:35:48 +0800

    improve the game

--- examples/flappy_bird/main.py
+++ examples/flappy_bird/main.py
@@ -3,20 +3,21 @@ import random
 class Jump():
     def __init__(self):
         super(Jump,self).__init__()
-        self.blockSize = 18
-        self.blockNum = 20
         self.barriers=[]
         self.barrier = []
-        self.playing = True
+        self.playing = False
+
         self.score = 0
-        self.block = [4,self.blockNum/2]
-
-
-        self.game = dkl.Game(self.blockNum * self.blockSize, self.blockNum * self.blockSize, "Jump")
-        self.game.fps = 8
+        self.game = dkl.Game(660,600,"Jump")
+        self.load = dkl.TextureDataLoader()
+        self.load.loadList([{'name':'bird','path':'./bird'}])
+        
         self.create()
+        self.create_bird()
         self.create_barrier()
+        self.start()
         self.game.run()
+        
     def create(self):
         self.stageLayer = dkl.Sprite()
         self.game.stage.addChild(self.stageLayer)
@@ -24,81 +25,106 @@ class Jump():
         self.stageLayer.addEventListener(dkl.LoopEvent.ENTER_FRAME, self.mainLoop)
 
         self.scoreTex = dkl.Texture()
-        self.scoreTex.x = self.scoreTex.y = 10
+        self.scoreTex.x = self.scoreTex.y = 1
         self.stageLayer.addChild(self.scoreTex)
-
         self.updateScoreText(self.score)
+
+    def start(self):
+        self.start = False
+
+        self.startTex = dkl.Texture(dkl.TextureData.fromText(" Please Enter 'SPACE' To Start ", size=40))
+        self.startTex.x = (self.game.windowWidth - self.startTex.getWidth()) / 2
+        self.startTex.y = 320
+        self.stageLayer.addChild(self.startTex)
+        
+
+    def create_bird(self):
+        self.bird = dkl.Texture(self.load.get('bird'))
+        self.bird.x = 4
+        self.bird.y = self.game.windowHeight/2
+        self.stageLayer.addChild(self.bird)
+
     def create_barrier(self):
-        for i in range(5):
-            a=random.randint(3,self.blockNum-3)
-            for e in range(self.blockNum):
-                if abs(e-a)>2:
-                    self.barrier.append([i*10+18,e])
+        for i in range(2):
+            a=random.randint(120,self.game.windowHeight-120)
+            for e in range(self.game.windowHeight):
+                if abs(e-a)>55:
+                    self.barrier.append([i*(self.game.windowWidth/2)+400,e])
             self.barriers.insert(0,self.barrier.copy())
             self.barrier.clear()
     
     def move(self):
         for i in self.barriers:
             for e in i:
-                e[0]-=1
-                if e[0]==1:
+                e[0]-=10
+                if e[0]<=0:
                     self.barriers.pop()
-                    a=random.randint(6,self.blockNum-6)
-                    for e in range(self.blockNum):
-                        if abs(e-a)>2:
-                            self.barrier.append([51,e])
+                    a=random.randint(120,self.game.windowHeight-120)
+                    for e in range(self.game.windowHeight):
+                        if abs(e-a)>55:
+                            self.barrier.append([self.game.windowWidth+10,e])
                     self.barriers.insert(0,self.barrier.copy())
                     self.barrier.clear()
                     self.updateScoreText(self.score+1)
                     break    
-        self.block[1]-=1
-        tx=self.block[0]
-        ty=self.block[1]
+        self.bird.y-=8
         for i in self.barriers:
-            if [tx,ty] in i or ty<=0 or ty>=self.blockNum:
+            if [self.bird.x+self.bird.getWidth()-2,self.bird.y+self.bird.getHeight()/2] in i or self.bird.y<=0 or self.bird.y>=self.game.windowHeight:
                 self.gameOver() 
         
     def onKeyDown(self, e):
         d = e.data
         if d["key"] == dkl.Keyboard.ESC:
             exit(0)
-        elif d["key"] == dkl.Keyboard.ENTER:
-            self.playing = not self.playing         
         elif d["key"] == dkl.Keyboard.F5:
             self.barriers=[]
             self.barrier=[]
             self.playing = True
             self.score = 0
-            self.block = [2,self.blockNum/2]
             self.stageLayer.removeChild(self.textTex)
+            self.stageLayer.removeChild(self.bird)
+            self.stageLayer.removeChild(self.hintTextTex)
+            self.create_bird()
             self.create_barrier()
+        if not self.playing:
+            if not self.start:
+                pass
+            else:
+                return
+        if d["key"] == dkl.Keyboard.ENTER:
+            self.playing = not self.playing         
+        
         elif d["key"] == dkl.Keyboard.SPACE:
-            self.block[1]+=2
+            if self.start == False:
+                self.start = True
+                self.playing = True
+                self.stageLayer.removeChild(self.startTex)
+            else:
+                self.bird.y+=50
     def updateScoreText(self, p):
         self.score = p
         self.scoreTex.textureData = dkl.TextureData.fromText("Score: %s" % self.score, size=20)
     def mainLoop(self, e):
         if not self.playing:
             return
-
         self.move()
-
         self.draw()
         
     def draw(self):
         self.stageLayer.graphics.clear()
-        for y in range(self.blockNum):
-            for x in range(self.blockNum):
-                if [x, y] == self.block:
-                    self.stageLayer.graphics.drawRect(x * self.blockSize, y * self.blockSize, self.blockSize, self.blockSize, fillStyle=dkl.Color.fromHex("#333333"))
-                for i in self.barriers:
-                    if [x,y] in i:
-                        self.stageLayer.graphics.drawRect(x * self.blockSize, y * self.blockSize, 5, self.blockSize, fillStyle=dkl.Color.fromHex("#FF0000"))
+        for i in self.barriers:
+            for e in i:
+                self.stageLayer.graphics.drawRect(e[0] , e[1] , 1, 1, fillStyle=dkl.Color.fromHex("#FF0000"))
     def gameOver(self):
         self.playing = False
-        self.textTex = dkl.Texture(dkl.TextureData.fromText("Game Over", size=50))
+        self.textTex = dkl.Texture(dkl.TextureData.fromText("Game Over", size=80))
         self.textTex.x = (self.game.windowWidth - self.textTex.getWidth()) / 2
-        self.textTex.y = 200
+        self.textTex.y = 340
         self.stageLayer.addChild(self.textTex)
+
+        self.hintTextTex = dkl.Texture(dkl.TextureData.fromText(">>Press 'F5' to restart<<", size = 40, color = dkl.Color.GREEN))
+        self.hintTextTex.x = (self.game.windowWidth - self.hintTextTex.getWidth()) / 2
+        self.hintTextTex.y = 260
+        self.stageLayer.addChild(self.hintTextTex)
 if __name__ == "__main__":
 	game = Jump()				
