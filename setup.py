2a6603f783a8744c6c227538591f9476493cc20c
yuehaowang <wangyuehao1999@gmail.com>
2018-01-19 22:07:13 +0800

    Update version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "duckling",
-	version = "1.0.0",
+	version = "1.0.0b1",
 	description = "An OpenGL based game engine for Python.",
 	author = "Yuehao Wang",
 	author_email = "wangyuehao1999@gmail.com",
