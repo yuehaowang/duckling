796f2ba26aaf1ae809f1767db7c7c5014a5d78d0
yuehaowang <wangyuehao1999@gmail.com>
2018-01-20 22:19:09 +0800

    Added PyInstaller support.

--- duckling/core/utils.py
+++ duckling/core/utils.py
@@ -10,6 +10,9 @@ class Path(object):
 		if os.path.isabs(path):
 			return path
 
-		mainPath = os.path.split(os.path.abspath(sys.argv[0]))[0]
+		if hasattr(sys, "_MEIPASS"):
+			rootPath = sys._MEIPASS
+		else:
+			rootPath = os.path.split(os.path.abspath(sys.argv[0]))[0]
 
-		return os.path.normpath(os.path.join(os.path.abspath(mainPath), path))
+		return os.path.normpath(os.path.join(rootPath, path))
